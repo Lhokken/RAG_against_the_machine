91a872e00fef562a27278934102ed14b92623db2
Gianluca Cerretelli <gcerrete@c1r4p8.42firenze.it>
2026-07-29 14:22:22 +0200

    index working

--- bm25_applier.py
+++ bm25_applier.py
@@ -6,6 +6,12 @@ import bm25s.tokenization
 
 
 class Bm25sApplier(BaseModel):
-	txt_tokens: list
 
 	def tokenizer(self, chunk_list: list[str]) -> None:
+		try:
+			chk_list_tokenized = bm25s.tokenize(chunk_list)
+			retriever = bm25s.BM25()
+			retriever.index(chk_list_tokenized)
+			retriever.save("Index_bm25s", corpus=chunk_list)
+		except ValidationError as e:
+			print(e)
--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
+import os
+import bm25s
 from explorer import Searcher
 from splitter import Splitter
+from bm25_applier import Bm25sApplier
 
 
 if __name__ == "__main__":
-	explorer = Searcher()
-	splitter = Splitter()
-	explorer.search_all()
-	# explorer.print_file_list()
-	splitter.split_all(explorer.file_list)
-	splitter.print_chunk_list()
+	if os.path.exists("Index_bm25s"):
+		print("Index already exist! Ultrafast loading.")
+		retriever = bm25s.BM25.load("Index_bm25s", load_corpus=True)
+		print(retriever.corpus)
+	else:
+		print("Index do not exist! Calculating ...")
+		explorer = Searcher()
+		splitter = Splitter()
+		explorer.search_all()
+		# explorer.print_file_list()
+		splitter.split_all(explorer.file_list)
+		splitter.print_chunk_list()
+		bm25_indexer = Bm25sApplier()
+		bm25_indexer.tokenizer(splitter.chunk_list_txt)
+		print("Index created and saved for next run!")
