ad053a74d60864d9844fe87a61201f0523511a02
Gianluca Cerretelli <gcerrete@c1r4p8.42firenze.it>
2026-08-26 20:05:26 +0200

    wip evaluate

--- src/data_models.py
+++ src/data_models.py
@@ -5,46 +5,57 @@ import uuid
 
 
 class MinimalSource(BaseModel):
+    """Class for chunk data"""
     file_path: str
     first_character_index: int
     last_character_index: int
 
 
 class UnansweredQuestion(BaseModel):
+    """Class for question without answer, assign unique identifier"""
     question_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     question: str
 
 
 class AnsweredQuestion(UnansweredQuestion):
+    """Class for question with answer"""
     sources: list[MinimalSource]
     answer: str
 
 
 class RagDataset(BaseModel):
+    """Class for dta block with answered and unanswered questions"""
     rag_questions: list[AnsweredQuestion | UnansweredQuestion]
 
 
 class MinimalSearchResults(BaseModel):
+    """Class for questions and N sources of file chunks"""
     question_id: str
     question: str
     retrieved_sources: list[MinimalSource]
 
 
 class MinimalAnswer(MinimalSearchResults):
+    """Class used to add answer to a MinimalSearchResults dataset"""
     answer: str
 
 
 class StudentSearchResults(BaseModel):
+    """Class to create a dataset of N questions and relative N chunks"""
     search_results: list[MinimalSearchResults]
     k: int
 
 
 class StudentSearchResultsAndAnswer(BaseModel):
+    """Class used to create a dataset of questions, answers and chunks"""
     search_results: list[MinimalAnswer]
     k: int
 
 
 class MinimalRagData(BaseModel):
+    """Class used to create a complete dataset
+    
+    with question, answer, chunks and other relevant data"""
     question_id: str
     question: str
     answer: str
@@ -54,4 +65,5 @@ class MinimalRagData(BaseModel):
 
 
 class RagDataGround(BaseModel):
+    """Class used to contain MinimalRagData"""
     rag_questions: list[MinimalRagData]
--- src/evaluate.py
+++ src/evaluate.py
@@ -4,9 +4,8 @@ import json
 from src.data_models import StudentSearchResults, RagDataGround
 
 
-def dict_student(
-        student_search_results_path: str
-        ) -> StudentSearchResults:
+def dict_student(student_search_results_path: str) -> StudentSearchResults:
+    """This function verify and return a student search result"""
     try:
         with open(student_search_results_path, encoding="utf-8") as source:
             result = StudentSearchResults.model_validate(json.load(source))
@@ -18,6 +17,7 @@ def dict_student(
 
 
 def dict_dataset(dataset_path: str) -> RagDataGround:
+    """This function verify and return a rag data ground_truth result"""
     try:
         with open(dataset_path, encoding="utf-8") as source:
             ground_truth = RagDataGround.model_validate(json.load(source))
@@ -31,40 +31,59 @@ def evaluation_step(
         student_search_results_path: str,
         dataset_path: str,
         k: int) -> None:
+    """This function build the recall@k result
+
+    In order to obtain the result, launche k times the
+    single evaluate function"""
     result = dict_student(student_search_results_path)
     ground_truth = dict_dataset(dataset_path)
-    ground_truth.rag_questions[0]
+    # ground_truth.rag_questions[0]
     n: int = 0
-    for elab in result.search_results:
-        # print(elab.question)
-        i = 0
-        id_elab = (elab.question_id)
-        while (i < len(ground_truth.rag_questions)):
-            if ground_truth.rag_questions[i].question_id == id_elab:
-                for source in elab.retrieved_sources[:k]:
-                    temp = ground_truth.rag_questions[i].sources[0]
-                    if source.file_path == temp.file_path:
-                        ground_first = temp.first_character_index
-                        ground_last = temp.last_character_index
-                        elab_first = source.first_character_index
-                        elab_last = source.last_character_index
-                        overlap = (
-                            min(ground_last, elab_last)
-                            - max(ground_first, elab_first))
-                        IoU = (overlap / (ground_last - elab_first))
-                        if IoU > 0.05:
-                            n += 1
-                            # print(f"{IoU:.2f}")
-                            # print(n)
-                            break
-            i += 1
-    rec = (n / len(result.search_results))
+    i: int = 0
+    j: int = 0
+    eval_result: list[list[str | float]] = []
+    while (j < k):
+        j = j + 1
+        n = 0
+        for elab in result.search_results:
+            # print(elab.question)
+            i = 0
+            id_elab = (elab.question_id)
+            while (i < len(ground_truth.rag_questions)):
+                if ground_truth.rag_questions[i].question_id == id_elab:
+                    for source in elab.retrieved_sources[:j]:
+                        temp = ground_truth.rag_questions[i].sources[0]
+                        if source.file_path == temp.file_path:
+                            ground_first = temp.first_character_index
+                            ground_last = temp.last_character_index
+                            elab_first = source.first_character_index
+                            elab_last = source.last_character_index
+                            overlap = (
+                                min(ground_last, elab_last)
+                                - max(ground_first, elab_first))
+                            IoU = (overlap / (ground_last - elab_first))
+                            if IoU > 0.05:
+                                n += 1
+                                # print(f"{IoU:.2f}")
+                                # print(n)
+                                break
+                i += 1
+        rec = (n / len(result.search_results))
+        eval_result.append([f"Recall@{j}: {rec:.3f}", (rec * 100)])
+
     valid: bool = True
+    print(eval_result[4])
     print(
         f"\nStudent data is valid: {valid}\n"
         f"Total number of questions: {len(result.search_results)}\n"
         "Total number of questions with sources: "
         f"{len(ground_truth.rag_questions)}\n"
-        f"Total number of questions with student sources: {n}\n"
-        f"Recall@{k}: {rec:.3f} ({(rec * 100):.1f}%)\n\n"
+        f"Total number of questions with student sources: {n}\n\n"
+        "Evaluation results"
+        "======================================\n"
+        f"Questions evaluated: {i}\n"
         )
+    for text in eval_result:
+
+        print(f"{text[0]} ({text[1]:.1f}%)")
+    print()
